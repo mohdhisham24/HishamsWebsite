7e98cc17e800118ab53b3fb3c88337fedcc3fe41
saleeqmohammed <saleeqmohammed7@gmail.com>
2024-02-04 15:52:17 +0000

    desktop version done

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,4 @@
 
-const text =document.querySelector('.text p');
-text.innerHTML = text.textContent.split('').map(
-  (char,i)=>`<span style="transform:rotate(${i*8.3}deg);">${char}</span> `
-).join('');
-
 
 const navLinks = document.querySelectorAll('.navlink');
 const sections = document.querySelectorAll('.section');
@@ -11,7 +6,7 @@ let activeSection = 'home';
 window.addEventListener('scroll',()=>{
 
   sections.forEach(element => {
-    if(window.scrollY >= (element.offsetTop-element.clientHeight/2)){
+    if(window.scrollY >= (element.offsetTop-element.clientHeight/1.6)){
       activeSection = element.id
     }
   });
@@ -25,3 +20,42 @@ window.addEventListener('scroll',()=>{
 
 
 });
+
+
+
+// function getRandomImage(){
+//   // get image containers
+  
+//   var imageContainers =[ document.getElementById('mar1'),document.getElementById('mar2'),document.getElementById('mar3')];
+
+//   imageContainers.forEach((imageContainer)=>{
+
+//       // Remove the first image
+//   var firstImage = imageContainer.firstElementChild;
+//   if (firstImage) {
+//     imageContainer.removeChild(firstImage);
+//   }
+
+
+
+//   var imageFiles = ['boat.jpg', 'confused.jpg', 'flowers.jpg', 'friends.jpg','manbeach.jpg','ullal.jpg'];
+
+//   // Get a random index from the array
+//   var randomIndex = Math.floor(Math.random() * imageFiles.length);
+
+//   // Get the random image file name
+//   var randomImageFile = imageFiles[randomIndex];
+
+//   // Insert a new image at the bottom
+//   var newImage = document.createElement('img');
+//   newImage.src = '/Art/' + randomImageFile;
+//   newImage.alt = 'New Image';
+//   imageContainer.appendChild(newImage);
+//   })
+// }
+
+// // JavaScript code to remove the first image and insert a new image at the bottom
+// document.addEventListener('DOMContentLoaded', function () {
+  
+//   setInterval(getRandomImage, 25000);
+// });
